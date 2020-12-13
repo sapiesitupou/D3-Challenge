1876de60388b9f15dce514b296d1abfefd11dc42
sapiesitupou <sapiesit@gmail.com>
2020-12-13 13:47:12 -0800

    linearscales

--- 16-D3/StarterCode/assets/js/app.js
+++ 16-D3/StarterCode/assets/js/app.js
@@ -21,4 +21,25 @@ let svg = chart.append('svg')
     .attr('width', svgWidth)
     .attr('height', svgHeight);
 
+let chartGroup = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+//identifying axis
+let chosenXAxis = 'poverty';
+let chosenYAxis = 'healthcare';
+
+//Linear Scales : xLinearScale
+function XScale(censusData, chosenXAxis){
+    xLinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8, d3.max(censusData, d => d[chosenXAxis])* 1.2])
+    .range([0, width]);
+    
+    return xLinearScale;
+}
+
+//Linear Scales : yLinearScale
+function yScale(censusData, chosenYAxis){
+    yLinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d[chosenYAxis]) * 0.8, d3.max(censusData, d => d[chosenYAxis]) * 1.2])
+    .range([height, 0])
+}
