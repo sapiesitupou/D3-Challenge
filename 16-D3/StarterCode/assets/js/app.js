2fb6e829d7ca678ab6b145e34c27c1bc21e00438
sapiesitupou <sapiesit@gmail.com>
2020-12-13 13:30:27 -0800

    creating chart

--- 16-D3/StarterCode/assets/js/app.js
+++ 16-D3/StarterCode/assets/js/app.js
@@ -1,1 +1,24 @@
 // @TODO: YOUR CODE HERE!
+//Setting up chart
+let svgWidth = 1000;
+let svgHeight = 600;
+
+let margin = {
+    top: 40,
+    right: 40,
+    bottom: 60;
+    left: 50
+};
+
+let width = svgWidth - margin.right - margin.left;
+let height = svgHeight - margin.top - margin.bottom;
+
+let chart = d3.select('#scatter')
+    .append('div')
+    .classed('chart', true);
+
+let svg = chart.append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+
+
